d2401e03726b1f7b4ce4f7193b330f8669bd969c
Oksana Kharun <oksana0202g@gmail.com>
2024-03-08 14:03:58 +0000

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,34 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
 import HttpError from '../helpers/HttpError.js';
+
 const { JWT_SECRET } = process.env;
 
 const authMiddleware = async (req, res, next) => {
-   const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
-        throw HttpError(401);
-    }
-
     try {
-        const { id } = jwt.verify(token, JWT_SECRET);
-        const user = await User.findById(id);
-        if (!user || !user.token) {
+        const { authorization = "" } = req.headers;
+        const [bearer, token] = authorization.split(" ");
+        if (bearer !== "Bearer") {
             throw HttpError(401);
         }
-        req.user = user;
-        next();
-    }
-    catch (error) {
-        next(HttpError(401, `"Not authorized"`));
-    }
+
+        try {
+            const { id } = jwt.verify(token, JWT_SECRET);
+            const user = await User.findById(id);
+            if (!user || user.token !== token) {
+                throw HttpError(401);
+            }
+
+            req.user = user;
+            next();
+        }
+        catch (error) {
+            next(HttpError(401, `"Not authorized"`));
+        }
+    } catch (error) {
+    next(error);
+  }
 }
+
 
 export default authMiddleware;
